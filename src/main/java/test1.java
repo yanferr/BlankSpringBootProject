e798e613db24143712c8759d82b1493ef7cd42b0
yanferrcode <859247755@qq.com>
2023-04-02 16:18:27 +0800

    bugFix commit

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -2,5 +2,7 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("git commit");
         System.out.println("git commit 2");
+        // commit branch merge rebase
+        System.out.println("bugFix");
     }
 }
